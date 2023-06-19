e0846b4e743b456d7525bb514fbfcdcf01b0e9d1
philipjk <filippo92galli@gmail.com>
2023-06-19 16:12:17 +0200

    fetch and log room temperature

--- logger.py
+++ logger.py
@@ -11,6 +11,7 @@ CHAT_ID = "-1001910393142"
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 TEMPERATURE_THRESHOLD = 75
 SEND_REQ = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+ROOM_SENSOR = "http://10.30.3.236/data"
 
 def push(filename):
     users_output = subprocess.check_output(["gpustat", "--json"])
@@ -18,6 +19,7 @@ def push(filename):
     t = output["query_time"]
     # use only day, hour and minute info
     t = t[5:16]
+    room_temp = get_room_temp()
     for gpu in output["gpus"]:
         users = []
         utilization = gpu['utilization.gpu']
@@ -26,17 +28,28 @@ def push(filename):
         # Append the GPU data to the CSV file
         with open(filename, mode='a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([t, gpu["index"], gpu["memory.used"], gpu["temperature.gpu"], utilization, users])
+            writer.writerow([t, gpu["index"], gpu["memory.used"], 
+                             gpu["temperature.gpu"], utilization,
+                             users, room_temp])
         if gpu["temperature.gpu"] > TEMPERATURE_THRESHOLD:
             usr_str = ', '.join(users)
             data = (f"""GPU {gpu['index']} is at {gpu['temperature.gpu']}°C. Alert user(s) {usr_str}.""")
-            requests.post(SEND_REQ, data={"chat_id": CHAT_ID, "text": data})
+            also = f" Room temperature is {room_temp}°C."
+            requests.post(SEND_REQ, data={"chat_id": CHAT_ID, "text": data+also})
+
+def get_room_temp():
+    try:
+        # get temp from ROOM_SENSOR, which returns a json
+        temp = requests.get(ROOM_SENSOR).json()['t']
+        return temp
+    except:
+        return None
 
 # Initialize the CSV file with a header row
 fname = time.strftime("%m-%d-%H-%M") + ".csv"
 with open(fname, mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['time', 'gpu_index', 'memory_used', 'temperature', 'utilization', 'users'])
+    writer.writerow(['time', 'gpu_index', 'memory_used', 'temperature', 'utilization', 'users', 'room_temp'])
 
 try:
     while True:
